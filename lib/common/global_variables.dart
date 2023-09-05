1f743f5a51a6a3c195e473cb0c9a8abe9470bd07
Tungky Adi Pradana <tungky.adi@uii.ac.id>
2023-09-05 19:01:03 +0700

    Change url global variable to fic7.tungkyap.my.id

--- lib/common/global_variables.dart
+++ lib/common/global_variables.dart
@@ -1,3 +1,4 @@
 class GlobalVariables {
-  static const baseUrl = 'https://c362-139-195-239-173.ngrok.io';
+  // static const baseUrl = 'https://c362-139-195-239-173.ngrok.io';
+  static const baseUrl = 'https://fic7.tungkyap.my.id';
 }
